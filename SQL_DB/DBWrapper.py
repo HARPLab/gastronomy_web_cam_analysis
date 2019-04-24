6f130705f940fd34dab7b5b926caeec0d9e9a122
roman <roman.a.kaufman@gmail.com>
2019-04-24 15:30:55 -0400

    fixing finally

--- SQL_DB/DBWrapper.py
+++ SQL_DB/DBWrapper.py
@@ -5,14 +5,14 @@ from SQL_DB.ClassDeclarations import Base, Clip, Frame, Pose, Object
 
 #also learned main logic for this wrapper [here](https://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/)
 class DBWrapper:
+    DB_ABSOLUTE_PATH='home/rkaufman/dev/gastronomy_web_cam_analysis/SQL_DB/parsed_clips.db'
     def __init__(self):
-        self.lit = True
-        engine = create_engine('sqlite:///parsed_clips.db')
+        self.engine = create_engine('sqlite:////{}'.format(DBWrapper.DB_ABSOLUTE_PATH))
         # Bind the engine to the metadata of the Base class so that the
         # declaratives can be accessed through a DBSession instance
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
-        self.session = DBSession()
+        Base.metadata.bind = self.engine
+        self.DBSession = sessionmaker(bind=self.engine)
+        self.session = self.DBSession()
 
     def get_session(self):
         return self.session
