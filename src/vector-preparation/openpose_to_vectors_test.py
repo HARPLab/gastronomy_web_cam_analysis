c260317e76939a6fa6ee33b4308be8c56ff00081
Ada Taylor <ada@idu.com>
2021-02-05 16:32:49 -0500

    Removed print statements from test suite

--- src/vector-preparation/openpose_to_vectors_test.py
+++ src/vector-preparation/openpose_to_vectors_test.py
@@ -19,9 +19,9 @@ class TestRoleAssignmentMethods(unittest.TestCase):
 			self.assertEqual(openpose_to_vectors.in_bd_box(bd_box_B, pt), correct_B[i])
 
 
-
-		
-
+	# The quality-checks folder contains images and their corresponding poses
+	# Both the selected A/B and all raw
+	# 
 	def test_which_pose(self):
 		filename = '8-21-18_shows_quality-check-random_f104051.json'
 		filename = 'quality-checks/' + filename
@@ -42,11 +42,6 @@ class TestRoleAssignmentMethods(unittest.TestCase):
 
 		test_a, test_b = openpose_to_vectors.get_role_assignments(all_poses_reshaped)
 
-		# print("Test A")
-		# print(test_a)
-
-		# print("Test B")
-		# print(test_b)
 		test_a = test_a.aslist()
 		test_b = test_b.aslist()
 
