12ecbb60f2efc33888e5719153acb247746ed0cd
Ada Taylor <ada@idu.com>
2021-02-14 16:49:17 -0500

    Added LSTM to experiment runner

--- src/experiments/experiment_runner.py
+++ src/experiments/experiment_runner.py
@@ -9,7 +9,23 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
+# from tensorflow import keras
+# from keras.models import Sequential
+# from tensorflow.keras.layers import LSTM
+# from tensorflow.keras.layers import Dense
+# from tensorflow.keras.layers import Dropout
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.utils import to_categorical
 
+activity_labels = ['away-from-table', 'idle', 'eating', 'drinking', 'talking', 'ordering', 'standing', 
+					'talking:waiter', 'looking:window', 'looking:waiter', 'reading:bill', 'reading:menu',
+					'paying:check', 'using:phone', 'using:napkin', 'using:purse', 'using:glasses',
+					'using:wallet', 'looking:PersonA', 'looking:PersonB', 'takeoutfood', 'leaving-table', 'cleaning-up', 'NONE']
 
 CLASSIFIER_ADABOOST = '_adaboost'
 CLASSIFIER_SGDC = '_sgdc'
@@ -18,8 +34,34 @@ CLASSIFIER_KNN9 = '_kNN9'
 CLASSIFIER_KNN5 = '_kNN5'
 CLASSIFIER_KNN3 = '_kNN3'
 CLASSIFIER_DecisionTree = '_dectree'
+CLASSIFIER_LSTM = '_lstm'
 
-def get_classifier(key):
+LSTM_NUM_LABELS = len(activity_labels)
+
+def get_LSTM(trainX, trainY):
+	print(trainX.shape)
+	print(trainY.shape)
+	# an input layer that expects 1 or more samples, 50 time steps, and 2 features
+	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], LSTM_NUM_LABELS
+	input_shape=(n_timesteps, n_features)
+
+	print("input_shape: " + str(input_shape))
+	print("ought to be (128 , (25*3)*#poses)")
+
+	dropout = 0.1
+	batch_size = 256
+	hidden_dim = 80*2
+	model = Sequential()
+	model.add(LSTM(hidden_dim, input_shape=input_shape))
+	model.add(Dropout(dropout))
+	model.add(Dense(n_outputs, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	# print("fiting model...." + model_name)
+	#plot_losses = TrainingPlot()
+	return model
+
+
+def get_classifier(key, X, Y):
 	if key == CLASSIFIER_ADABOOST:
 		return AdaBoostClassifier()
 	if key == CLASSIFIER_SGDC:
@@ -34,6 +76,8 @@ def get_classifier(key):
 		return KNeighborsClassifier(n_neighbors=9)
 	if key == CLASSIFIER_DecisionTree:
 		return DecisionTreeClassifier()
+	if key == CLASSIFIER_LSTM:
+		return get_LSTM(X, Y)
 
 	return None
 
@@ -51,10 +95,23 @@ def classifier_train(X, Y, classifier_key):
 	print(Y)
 	print("Building " + classifier_key)
 	# classifier = KNeighborsClassifier(n_neighbors=9)
-	classifier = get_classifier(classifier_key)
+	classifier = get_classifier(classifier_key, X, Y)
 
 	time_start = time.perf_counter()
-	classifier.fit(X, Y)
+	
+	if classifier_key == CLASSIFIER_LSTM:
+		# Y.shape[]
+		dropout = 0.1
+		batch_size = 256
+		hidden_dim = 80
+		epochs = 70
+		Y = to_categorical(Y, num_classes=len(activity_labels))
+		classifier.fit(X, Y, batch_size=batch_size, verbose=0)
+	else:
+		classifier.fit(X, Y)
+
+
+
 
 	# clf.fit(X, Y)
 	time_end = time.perf_counter()
@@ -63,11 +120,65 @@ def classifier_train(X, Y, classifier_key):
 
 	return classifier
 
-def classifier_test(classifier, X, Y):
+def get_single_vector_of_multiclass_result(result):
+	decoded_result = result.argmax(axis=1)
+	return decoded_result
+
+#     frequencytest = {}
+#     frequencypred = {}
+#     for num in decodedtestY:
+#             if num not in frequencytest.keys():
+#                     frequencytest[num] = 1
+#             else:
+#                     frequencytest[num] = frequencytest[num] + 1
+#     for num in decodedpredY:
+#             if num not in frequencypred.keys():
+#                     frequencypred[num] = 1
+#             else:
+#                     frequencypred[num] = frequencypred[num] + 1
+#     print("stats:")
+#     print(frequencytest)
+#     print(frequencypred)
+#     self.logfile.write("cm stats:\n")
+#     self.logfile.write(str(frequencytest) + "\n")
+#     self.logfile.write(str(frequencypred) + "\n")
+#     #print(decodedpredY.shape)
+#     #print(decodedtestY.shape)
+#     predPadding = []
+#     testPadding = []
+#     i = 0
+#     for key in activitydict.keys():
+#             #print(key)
+#             predPadding.append(i)
+#             testPadding.append(23-i)
+#             i +=1
+#     decodedpredY = np.append(decodedpredY, predPadding)
+#     decodedtestY = np.append(decodedtestY, testPadding)
+#     cm = confusion_matrix(decodedtestY,decodedpredY)
+#     np.set_printoptions(precision=2)
+#     fig, ax = plt.subplots()
+#     sum_of_rows = cm.sum(axis=1)
+#     cm = cm / (sum_of_rows[:, np.newaxis]+1e-8)
+#     p, r = calc_precision_recall(cm,self.logfile)
+#     pickle.dump(cm, open(filename +"cm_mat.p", "wb"))
+#     plot_confusion_matrix(cm,cmap=plt.cm.Blues)
+#     plt.savefig(filename + "cm.png")
+#     plt.close()
+
+# 	return result
+
+def classifier_test(classifier, X, Y, classifier_type):
 	print(X.shape)
 	print("Predicting...")
 	time_start = time.perf_counter()
 	result = classifier.predict(X)
+	print(result.shape)
+
+	if classifier_type == CLASSIFIER_LSTM:
+		result = get_single_vector_of_multiclass_result(result)
+
+	# print(result)
+	# print(result.shape)
 	time_end = time.perf_counter()
 	time_diff = time_end - time_start
 	print("Time elapsed: " + str(time_diff))
@@ -85,14 +196,62 @@ def unpack_dict(input_set):
 	return X_train, X_test, Y_train, Y_test
 
 
+# def get_AlB(X_array, Y_array):
+# 	og_dim_X = X_array.shape
+# 	og_dim_Y = Y_array.shape
+
+# 	half_dim_X = int(og_dim_X[1] / 2)
+# 	half_dim_Y = int(og_dim_Y[1] / 2)
+
+# 	Xa = X_array[:, :half_dim_X]
+# 	lb = Y_array[:, 	half_dim_Y:]
+
+# 	X_alb = np.hstack((Xa, lb))
+
+# 	return X_alb, Y_array[:, :half_dim_Y]
+
+# def get_BlA(X_array, Y_array):
+# 	og_dim_X = X_array.shape
+# 	og_dim_Y = Y_array.shape
+
+# 	half_dim_X = int(og_dim_X[1] / 2)
+# 	half_dim_Y = int(og_dim_Y[1] / 2)
+
+# 	Xb = X_array[:, half_dim_X:]
+# 	la = Y_array[:, 	:half_dim_Y]
+
+# 	X_bla = np.hstack((Xb, la))
+
+# 	return X_bla, Y_array[:, :half_dim_Y]
+
+
+# def get_A(X_array, Y_array):
+# 	og_dim_X = X_array.shape
+# 	og_dim_Y = Y_array.shape
+
+# 	half_dim_X = int(og_dim_X[1] / 2)
+# 	half_dim_Y = int(og_dim_Y[1] / 2)
+
+# 	return X_array[:, :half_dim_X], Y_array[:, 	:half_dim_Y]
+
+# def get_B(X_array, Y_array):
+# 	og_dim_X = X_array.shape
+# 	og_dim_Y = Y_array.shape
+
+# 	half_dim_X = int(og_dim_X[1] / 2)
+# 	half_dim_Y = int(og_dim_Y[1] / 2)
+
+# 	return X_array[:, half_dim_X:], Y_array[:, 	half_dim_Y:]
+
+
 def get_AlB(X_array, Y_array):
 	og_dim_X = X_array.shape
 	og_dim_Y = Y_array.shape
 
-	half_dim_X = int(og_dim_X[1] / 2)
+	half_dim_X = int(og_dim_X[2] / 2)
 	half_dim_Y = int(og_dim_Y[1] / 2)
 
-	Xa = X_array[:, :half_dim_X]
+	Xa = X_array[:, :, :half_dim_X]
 	lb = Y_array[:, 	half_dim_Y:]
 
 	X_alb = np.hstack((Xa, lb))
@@ -103,11 +262,11 @@ def get_BlA(X_array, Y_array):
 	og_dim_X = X_array.shape
 	og_dim_Y = Y_array.shape
 
-	half_dim_X = int(og_dim_X[1] / 2)
+	half_dim_X = int(og_dim_X[2] / 2)
 	half_dim_Y = int(og_dim_Y[1] / 2)
 
-	Xb = X_array[:, half_dim_X:]
-	la = Y_array[:, 	:half_dim_Y]
+	Xb = X_array[:, :, half_dim_X:]
+	la = Y_array[:, :half_dim_Y]
 
 	X_bla = np.hstack((Xb, la))
 
@@ -118,19 +277,20 @@ def get_A(X_array, Y_array):
 	og_dim_X = X_array.shape
 	og_dim_Y = Y_array.shape
 
-	half_dim_X = int(og_dim_X[1] / 2)
+	half_dim_X = int(og_dim_X[2] / 2)
 	half_dim_Y = int(og_dim_Y[1] / 2)
 
-	return X_array[:, :half_dim_X], Y_array[:, 	:half_dim_Y]
+	return X_array[:, :, :half_dim_X], Y_array[:, 	:half_dim_Y]
 
 def get_B(X_array, Y_array):
 	og_dim_X = X_array.shape
 	og_dim_Y = Y_array.shape
 
-	half_dim_X = int(og_dim_X[1] / 2)
+	half_dim_X = int(og_dim_X[2] / 2)
 	half_dim_Y = int(og_dim_Y[1] / 2)
 
-	return X_array[:, half_dim_X:], Y_array[:, 	half_dim_Y:]
+	return X_array[:, :, half_dim_X:], Y_array[:, 	half_dim_Y:]
+
 
 
 def experiment_duo_vs_solo_swapped(vector_dict, unique_title, classifier_type, exp_batch_id):
@@ -154,11 +314,11 @@ def experiment_duo_vs_solo_swapped(vector_dict, unique_title, classifier_type, e
 		X_test_B, 	Y_test_B 	= get_B(X_test_AB, Y_test_AB)
 
 		svm_a_b = classifier_train(X_train_A, Y_train_B, classifier_type)
-		result_a_b = classifier_test(svm_a_b, X_test_A, Y_test_B)
+		result_a_b = classifier_test(svm_a_b, X_test_A, Y_test_B, classifier_type)
 		export_result(result_a_b, long_prefix, label_a_b)
 
 		svm_b_a = classifier_train(X_train_B, Y_train_A, classifier_type)
-		result_b_a = classifier_test(svm_b_a, X_test_B, Y_test_A)
+		result_b_a = classifier_test(svm_b_a, X_test_B, Y_test_A, classifier_type)
 		export_result(result_b_a, long_prefix, label_b_a)
 
 		# store for future examination
@@ -195,11 +355,11 @@ def experiment_label_to_label(vector_dict, unique_title, classifier_type, exp_ba
 		X_test_B, 	Y_test_B 	= get_B(X_test_AB, Y_test_AB)
 
 		svm_la_lb = classifier_train(Y_train_A, Y_train_B, classifier_type)
-		result_la_lb = classifier_test(svm_la_lb, Y_test_A, Y_test_B)
+		result_la_lb = classifier_test(svm_la_lb, Y_test_A, Y_test_B, classifier_type)
 		export_result(result_la_lb, long_prefix, label_la_lb)
 
 		svm_lb_la = classifier_train(Y_train_B, Y_train_A, classifier_type)
-		result_lb_la = classifier_test(svm_lb_la, Y_test_B, Y_test_A)
+		result_lb_la = classifier_test(svm_lb_la, Y_test_B, Y_test_A, classifier_type)
 		export_result(result_lb_la, long_prefix, label_lb_la)
 
 		# store for future examination
@@ -236,11 +396,11 @@ def experiment_duo_vs_solo_just_labels(vector_dict, unique_title, classifier_typ
 		X_test_BlA, Y_test_B 		= get_BlA(X_test_AB, Y_test_AB)
 
 		svm_alb_a = classifier_train(X_train_AlB, Y_train_A, classifier_type)
-		result_alb_a = classifier_test(svm_alb_a, X_test_AlB, Y_test_A)
+		result_alb_a = classifier_test(svm_alb_a, X_test_AlB, Y_test_A, classifier_type)
 		export_result(result_alb_a, long_prefix, label_alb_a)
 
 		svm_bla_b = classifier_train(X_train_BlA, Y_train_B, classifier_type)
-		result_bla_b = classifier_test(svm_bla_b, X_test_BlA, Y_test_B)
+		result_bla_b = classifier_test(svm_bla_b, X_test_BlA, Y_test_B, classifier_type)
 		export_result(result_bla_b, long_prefix, label_bla_b)
 
 		# store for future examination
@@ -287,20 +447,20 @@ def experiment_duo_vs_solo(vector_dict, unique_title, classifier_type, exp_batch
 
 
 		svm_a_a = classifier_train(X_train_A, Y_train_A, classifier_type)
-		result_a_a = classifier_test(svm_a_a, X_test_A, Y_test_A)
+		result_a_a = classifier_test(svm_a_a, X_test_A, Y_test_A, classifier_type)
 		export_result(result_a_a, long_prefix, label_a_a)
 
 		svm_b_b = classifier_train(X_train_B, Y_train_B, classifier_type)
-		result_b_b = classifier_test(svm_b_b, X_test_B, Y_test_B)
+		result_b_b = classifier_test(svm_b_b, X_test_B, Y_test_B, classifier_type)
 		export_result(result_b_b, long_prefix, label_b_b)
 
 		svm_ab_a = classifier_train(X_train_AB, Y_train_A, classifier_type)
-		result_ab_a = classifier_test(svm_ab_a, X_test_AB, Y_test_A)
+		result_ab_a = classifier_test(svm_ab_a, X_test_AB, Y_test_A, classifier_type)
 		export_result(result_ab_a, long_prefix, label_ab_a)
 
 
 		svm_ab_b = classifier_train(X_train_AB, Y_train_B, classifier_type)
-		result_ab_b = classifier_test(svm_ab_b, X_test_AB, Y_test_B)
+		result_ab_b = classifier_test(svm_ab_b, X_test_AB, Y_test_B, classifier_type)
 		export_result(result_ab_b, long_prefix, label_ab_b)
 
 		
@@ -322,11 +482,13 @@ def experiment_duo_vs_solo(vector_dict, unique_title, classifier_type, exp_batch
 
 
 # Given a file location, return the four test/train vectors
-def import_vectors(unique_title, prefix, fold_id):
+def import_vectors(unique_title, prefix, fold_id, dimension_X_row):
 	entries = os.listdir(prefix)
 
 	# get all the input files from this video
 	entries = list(filter(lambda k: unique_title in k, entries))
+
+	entries = list(filter(lambda k: 'total' in k, entries))
 
 	fold_group = "f" + str(fold_id) + "_"
 	fold_entries = list(filter(lambda k: fold_group in k, entries))
@@ -343,6 +505,9 @@ def import_vectors(unique_title, prefix, fold_id):
 		print("Error in import: multiple matching batches for this unique key")
 		print("Please provide a key that aligns with only one of the following")
 		print(X_test)
+	if len(X_test_label) == 0 or len(Y_test_label) == 0  or len(X_train_label) == 0  or len(Y_train_label) == 0:
+		print("Sorry, no import matching found")
+		print(fold_group)
 
 	X_test_label 	= X_test_label[0]
 	X_train_label 	= X_train_label[0]
@@ -354,8 +519,11 @@ def import_vectors(unique_title, prefix, fold_id):
 	Y_test 		= pickle.load(open(prefix + Y_test_label, 'rb'))
 	Y_train 	= pickle.load(open(prefix + Y_train_label, 'rb'))
 
-	X_test 		= X_test.reshape(X_test.shape[0], 50*3)
-	X_train 	= X_train.reshape(X_train.shape[0], 50*3)
+	window_size = 128
+	n_features = 2*25*3
+
+	X_test 		= X_test.reshape(X_test.shape[0], window_size, n_features)
+	X_train 	= X_train.reshape(X_train.shape[0], window_size, n_features) # dimension_X_row)
 	
 	return X_train, X_test, Y_train, Y_test
 
@@ -365,13 +533,14 @@ def get_svm_vectors(folds, unique_title, seed=42):
 	# These variables are set for a given import
 	# different seeds, different values
 	
-	prefix = '../vector-preparation/output-vectors/for_svm/'
+	prefix = '../vector-preparation/output-vectors/stateless/'
 	
 	exp_sets = {}
 	# exp_sets['all'] = import_vectors(unique_title, prefix, -1)
 	for fold_id in range(folds):
 		print("Geting svm data for fold " + str(fold_id))
-		X_train, X_test, Y_train, Y_test = import_vectors(unique_title, prefix, fold_id)
+		dimension_X_row = 2*25*3
+		X_train, X_test, Y_train, Y_test = import_vectors(unique_title, prefix, fold_id, dimension_X_row)
 
 		export_result(Y_train, 	'results/', 'Ytruetrain_f' + str(fold_id))
 		export_result(Y_test, 	'results/', 'Ytruetest_f' + str(fold_id))	
@@ -380,15 +549,42 @@ def get_svm_vectors(folds, unique_title, seed=42):
 
 	return exp_sets
 
+
+# Returns each set of 
+def get_temporal_vectors(folds, unique_title, seed=42):
+	# These variables are set for a given import
+	# different seeds, different values
+	
+	prefix = '../vector-preparation/output-vectors/temporal/'
+	
+	exp_sets = {}
+	# exp_sets['all'] = import_vectors(unique_title, prefix, -1)
+	for fold_id in range(folds):
+		print("Geting temporal data for fold " + str(fold_id))
+		dimension_X_row = (128,2*25*3)
+
+		X_train, X_test, Y_train, Y_test = import_vectors(unique_title, prefix, fold_id, dimension_X_row)
+
+		export_result(Y_train, 	'results/', 'Ytruetrain_f' + str(fold_id))
+		export_result(Y_test, 	'results/', 'Ytruetest_f' + str(fold_id))	
+
+		exp_sets[folds] = {'xtest': X_test, 'xtrain': X_train, 'ytest': Y_test, 'ytrain': Y_train}
+
+	return exp_sets
+
+
 def run_experiments():
 	folds = 5
-	unique_title = 'total_forsvm_s42_'
+	unique_title = 's42_'
 
-	exp_batch_id = 2
+	# 3 = hidden dim 80
+	# 4 = hidden dim 160
+	exp_batch_id = 4
 	exp_batch_id = "exp_" + str(exp_batch_id) + "/"
 	prefix_export = 'results/' + exp_batch_id
 
-	all_svm_vectors = get_svm_vectors(folds, unique_title)
+	# all_svm_vectors = get_svm_vectors(folds, unique_title)
+	all_temporal_vectors = get_temporal_vectors(folds, unique_title)
 
 	try:
 		os.mkdir(prefix_export)  
@@ -396,7 +592,7 @@ def run_experiments():
 		print("This directory already exists; do you want a fresh experiment ID?")
 
 	# exp_types = [CLASSIFIER_DecisionTree, CLASSIFIER_KNN3, CLASSIFIER_KNN9, CLASSIFIER_ADABOOST, CLASSIFIER_SGDC]
-	exp_types = [CLASSIFIER_SVM, CLASSIFIER_SGDC]
+	exp_types = [CLASSIFIER_LSTM]
 
 	# for i in range(len(exp_types)):		
 	# 	classifier_type = exp_types[i]
@@ -404,15 +600,21 @@ def run_experiments():
 	for i in range(len(exp_types)):		
 		classifier_type = exp_types[i]
 
-		experiment_duo_vs_solo_swapped(all_svm_vectors, unique_title, classifier_type, exp_batch_id)
-		experiment_duo_vs_solo_just_labels(all_svm_vectors, unique_title, classifier_type, exp_batch_id)
-		experiment_label_to_label(all_svm_vectors, unique_title, classifier_type, exp_batch_id)
-		experiment_duo_vs_solo(all_svm_vectors, unique_title, classifier_type, exp_batch_id)
+		train_test_vectors = None
+		if classifier_type == CLASSIFIER_LSTM:
+			train_test_vectors = all_temporal_vectors
+		else:
+			train_test_vectors = all_svm_vectors
+
+		experiment_duo_vs_solo_swapped(train_test_vectors, unique_title, classifier_type, exp_batch_id)
+		# experiment_duo_vs_solo_just_labels(train_test_vectors, unique_title, classifier_type, exp_batch_id)
+		# experiment_label_to_label(train_test_vectors, unique_title, classifier_type, exp_batch_id)
+		experiment_duo_vs_solo(train_test_vectors, unique_title, classifier_type, exp_batch_id)
 
 
 
 def main():
-    run_experiments()
+	run_experiments()
 
 
 main()
