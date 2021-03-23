08aefac7ae84c2e74afaff78184057ff6a8b3d79
Ada Taylor <ada@idu.com>
2021-03-23 00:43:46 -0400

    Added translations for reducing Y labels

--- src/arconsts.py
+++ src/arconsts.py
@@ -27,10 +27,12 @@ BATCH_ID_TEMPORAL 	= '_temporal'
 BATCH_ID_MEALWISE_STATELESS 	= '_mwstateless'
 BATCH_ID_MEALWISE_TEMPORAL 	= '_mwtemporal'
 
-FEATURES_VANILLA  = '_fvan'
-FEATURES_OFFSET   = '_foffset'
-FEATURES_ANGLES   = '_fang'
-FEATURES_NO_PROB  = '_fnprob'
+FEATURES_VANILLA      = '_fvanilla'
+FEATURES_OFFSET       = '_foffset'
+FEATURES_ANGLES       = '_fangles'
+FEATURES_NO_PROB      = '_fnoprob'
+FEATURES_LABELS_FULL    = '_ffull'
+FEATURES_LABELS_REDUCED = '_freduced'
 
 CONST_WINDOW_SIZE = 128
 
@@ -49,14 +51,14 @@ activitydict = {0: 'NONE', 1: 'away-from-table', 2: 'idle', 3: 'eating', 4: 'tal
 	7: 'reading:bill', 8: 'reading:menu', 9: 'paying:check', 10: 'using:phone', 11: 'using:objs', 12: 'standing'}
 
 activity_from_key = {0:'away-from-table', 1:'idle', 2:'eating', 3: 'drinking', 4: 'talking', 5: 'ordering', 6: 'standing',
-                        7: 'talking:waiter', 8: 'looking:window', 9: 'looking:waiter', 10: 'reading:bill', 11: 'reading:menu',
-                        12: 'paying:check', 13: 'using:phone', 14: 'using:napkin', 15: 'using:purse', 16: 'using:glasses',
-                        17: 'using:wallet', 18: 'looking:PersonA', 19: 'looking:PersonB', 20: 'takeoutfood', 21: 'leaving-table', 22: 'cleaning-up', 23: 'NONE'}
+                7: 'talking:waiter', 8: 'looking:window', 9: 'looking:waiter', 10: 'reading:bill', 11: 'reading:menu',
+                12: 'paying:check', 13: 'using:phone', 14: 'using:napkin', 15: 'using:purse', 16: 'using:glasses',
+                17: 'using:wallet', 18: 'looking:PersonA', 19: 'looking:PersonB', 20: 'takeoutfood', 21: 'leaving-table', 22: 'cleaning-up', 23: 'NONE'}
 
 activitydict_full_text_to_id = {'away-from-table': 0, 'idle': 1, 'eating': 2, 'drinking': 3, 'talking': 4, 'ordering': 5, 'standing':6,
-            'talking:waiter': 7, 'looking:window': 8, 'looking:waiter': 9, 'reading:bill':10, 'reading:menu': 11,
-            'paying:check': 12, 'using:phone': 13, 'using:napkin': 14, 'using:purse': 15, 'using:glasses': 16,
-            'using:wallet': 17, 'looking:PersonA': 18, 'looking:PersonB':19, 'takeoutfood':20, 'NONE': 21}
+        'talking:waiter': 7, 'looking:window': 8, 'looking:waiter': 9, 'reading:bill':10, 'reading:menu': 11,
+        'paying:check': 12, 'using:phone': 13, 'using:napkin': 14, 'using:purse': 15, 'using:glasses': 16,
+        'using:wallet': 17, 'looking:PersonA': 18, 'looking:PersonB':19, 'takeoutfood':20, 'NONE': 21}
 
 activity_from_key_full = activity_from_key
 
@@ -66,8 +68,8 @@ CONST_NUM_SUBPOINTS = 3
 CONST_NUM_LABEL = 1
 
 keypoint_labels = ["Nose","Neck","RShoulder","RElbow","RWrist","LShoulder",
-                                                "LElbow","LWrist","MidHip","RHip","RKnee","RAnkle","LHip","LKnee","LAnkle","REye","LEye","REar",
-                                                "LEar","LBigToe", "LSmallToe", "LHeel", "RBigToe", "RSmallToe", "RHeel", "Background", '']
+                                "LElbow","LWrist","MidHip","RHip","RKnee","RAnkle","LHip","LKnee","LAnkle","REye","LEye","REar",
+                                "LEar","LBigToe", "LSmallToe", "LHeel", "RBigToe", "RSmallToe", "RHeel", "Background", '']
 
 filenames_all = ['8-13-18', '8-18-18', '8-17-18', '8-21-18', '8-9-18']
 filenames_shifted = ['8-9-18']
@@ -91,14 +93,14 @@ PD_POSE_A_RAW     = 'pose_A_raw'
 PD_POSE_B_RAW     = 'pose_B_raw'
 PD_VALIDITY_STATE = 'validity_state'
 
-PD_POSE_A_WINDOWED      = 'pose_A_windowed'
-PD_POSE_B_WINDOWED      = 'pose_B_windowed'
-PD_INDEX_START          = 'index_start'
-PD_INDEX_END            = 'index_end'
-PD_TEST_SET             = 'test_set'
+PD_POSE_A_WINDOWED    = 'pose_A_windowed'
+PD_POSE_B_WINDOWED    = 'pose_B_windowed'
+PD_INDEX_START        = 'index_start'
+PD_INDEX_END        = 'index_end'
+PD_TEST_SET         = 'test_set'
 
-PD_COLS_FEAT_QUAL_CHECKS      = [PD_MEALID, PD_FRAMEID, PD_LABEL_A_RAW, PD_LABEL_B_RAW, PD_LABEL_A_CODE, PD_LABEL_B_CODE, PD_POSE_A_RAW, PD_POSE_B_RAW, PD_VALIDITY_STATE]
-PD_COLS_SLICES                = [PD_MEALID, PD_LABEL_A_RAW, PD_LABEL_B_RAW, PD_INDEX_START, PD_INDEX_END, PD_TEST_SET]
+PD_COLS_FEAT_QUAL_CHECKS    = [PD_MEALID, PD_FRAMEID, PD_LABEL_A_RAW, PD_LABEL_B_RAW, PD_LABEL_A_CODE, PD_LABEL_B_CODE, PD_POSE_A_RAW, PD_POSE_B_RAW, PD_VALIDITY_STATE]
+PD_COLS_SLICES            = [PD_MEALID, PD_LABEL_A_RAW, PD_LABEL_B_RAW, PD_INDEX_START, PD_INDEX_END, PD_TEST_SET]
 
 
 bd_box_A = ((70, 80), (200, 340))
@@ -121,74 +123,179 @@ LABEL_A_B = 'a_b'
 
 CSV_ORDER = ['a_a', 'ab_a', 'alb_a', 'b_a', 'b_b', 'ab_b', 'bla_b', 'a_b', 'la_lb', 'lb_la']
 
+OG_LABEL_NA                   = -1
+OG_LABEL_AWAY_FROM_TABLE      = 0
+OG_LABEL_IDLE                 = 1
+OG_LABEL_EATING               = 2
+OG_LABEL_DRINKING             = 3
+OG_LABEL_TALKING              = 4
+OG_LABEL_ORDERING             = 5
+OG_LABEL_STANDING             = 6
+OG_LABEL_TALKING_WAITER       = 7
+OG_LABEL_LOOKING_WINDOW       = 8
+OG_LABEL_LOOKING_WAITER       = 9
+OG_LABEL_READING_BILL         = 10
+OG_LABEL_READING_MENU         = 11
+OG_LABEL_PAYING_CHECK         = 12
+OG_LABEL_USING_PHONE          = 13
+OG_LABEL_USING_NAPKIN         = 14
+OG_LABEL_USING_PURSE          = 15
+OG_LABEL_USING_GLASSES        = 16
+OG_LABEL_USING_WALLET         = 17
+OG_LABEL_LOOKING_PERSON_A     = 18
+OG_LABEL_LOOKING_PERSON_B     = 19
+OG_LABEL_TAKEOUT_FOOD         = 20
+OG_LABEL_LEAVING_TABLE        = 21
+OG_LABEL_CLEANING_UP          = 22
+OG_LABEL_NONE                 = 23
+
+
+ACT_NONE                = 0
+ACT_AWAY_FROM_TABLE     = 1
+ACT_IDLE                = 2
+ACT_EATING              = 3
+ACT_TALKING             = 4
+ACT_WAITER              = 5
+ACT_LOOKING_WINDOW      = 6
+ACT_READING_BILL        = 7
+ACT_READING_MENU        = 8
+ACT_PAYING_CHECK        = 9
+ACT_USING_PHONE         = 10
+ACT_OBJ_WILDCARD        = 11
+ACT_STANDING            = 12
+ACT_LEAVING             = 13
+ACT_DRINKING            = 14
+
+LEN_REDUCED_ALL = range(ACT_DRINKING)
+NUM_REDUCED_ALL = 15
+LEN_OG_ALL = range(OG_LABEL_NONE)
+NUM_NOT_REDUCED = 24
+
+transform_dict = {}
+transform_dict[OG_LABEL_NA]               = ACT_NONE
+transform_dict[OG_LABEL_AWAY_FROM_TABLE]  = ACT_NONE    
+transform_dict[OG_LABEL_IDLE]             = ACT_IDLE
+transform_dict[OG_LABEL_EATING]           = ACT_EATING
+transform_dict[OG_LABEL_DRINKING]         = ACT_DRINKING
+transform_dict[OG_LABEL_TALKING]          = ACT_TALKING
+transform_dict[OG_LABEL_ORDERING]         = ACT_WAITER
+transform_dict[OG_LABEL_STANDING]         = ACT_STANDING
+transform_dict[OG_LABEL_TALKING_WAITER]   = ACT_WAITER
+transform_dict[OG_LABEL_LOOKING_WINDOW]   = ACT_LOOKING_WINDOW
+transform_dict[OG_LABEL_LOOKING_WAITER]   = ACT_WAITER
+transform_dict[OG_LABEL_READING_BILL]     = ACT_READING_BILL
+transform_dict[OG_LABEL_READING_MENU]     = ACT_READING_MENU
+transform_dict[OG_LABEL_PAYING_CHECK]     = ACT_PAYING_CHECK
+transform_dict[OG_LABEL_USING_PHONE]      = ACT_USING_PHONE
+transform_dict[OG_LABEL_USING_NAPKIN]     = ACT_OBJ_WILDCARD
+transform_dict[OG_LABEL_USING_PURSE]      = ACT_OBJ_WILDCARD
+transform_dict[OG_LABEL_USING_GLASSES]    = ACT_OBJ_WILDCARD
+transform_dict[OG_LABEL_USING_WALLET]     = ACT_OBJ_WILDCARD
+transform_dict[OG_LABEL_LOOKING_PERSON_A] = ACT_IDLE
+transform_dict[OG_LABEL_LOOKING_PERSON_B] = ACT_IDLE
+transform_dict[OG_LABEL_TAKEOUT_FOOD]     = ACT_OBJ_WILDCARD
+transform_dict[OG_LABEL_LEAVING_TABLE]    = ACT_LEAVING
+transform_dict[OG_LABEL_CLEANING_UP]      = ACT_LEAVING
+transform_dict[OG_LABEL_NONE]             = ACT_NONE
+
 
 def label_encode(label_string):
-      if label_string in activitydict_full_text_to_id:
-            return activitydict_full_text_to_id[label_string]
+    if label_string in activitydict_full_text_to_id:
+        return activitydict_full_text_to_id[label_string]
 
-      return -1
-
-
-def reduce_labels(Y_array):
-      # activity_from_key = {0:'away-from-table', 1:'idle', 2:'eating', 3: 'drinking', 4: 'talking', 5: 'ordering', 6: 'standing',
-      #                             7: 'talking:waiter', 8: 'looking:window', 9: 'looking:waiter', 10: 'reading:bill', 11: 'reading:menu',
-      #                             12: 'paying:check', 13: 'using:phone', 14: 'using:napkin', 15: 'using:purse', 16: 'using:glasses',
-      #                             17: 'using:wallet', 18: 'looking:PersonA', 19: 'looking:PersonB', 20: 'takeoutfood', 21: 'leaving-table', 22: 'cleaning-up', 23: 'NONE'}
-
-      # activity_labels = [0: 'NONE', 1: 'away-from-table', 2: 'idle', 3: 'eating', 4: 'talking', 5:'talking:waiter', 6: 'looking:window', 
-      # 7: 'reading:bill', 8: 'reading:menu', 9: 'paying:check', 10: 'using:phone', 11: 'using:objs', 12: 'standing']
-      
-      ACT_NONE                = 0
-      ACT_AWAY_FROM_TABLE     = 1
-      ACT_IDLE                = 2
-      ACT_EATING              = 3
-      ACT_TALKING             = 4
-      ACT_WAITER              = 5
-      ACT_LOOKING_WINDOW      = 6
-      ACT_READING_BILL        = 7
-      ACT_READING_MENU        = 8
-      ACT_PAYING_CHECK        = 9
-      ACT_USING_PHONE         = 10
-      ACT_OBJ_WILDCARD  = 11
-      ACT_STANDING            = 12
+    return -1
 
 
-      Y_new = np.empty_like(Y_array)
-      Y_new = np.where(Y_array==23, ACT_NONE,         Y_new)
-      Y_new = np.where(Y_array==22, ACT_NONE,         Y_new)
-      Y_new = np.where(Y_array==21, ACT_STANDING,     Y_new) 
-      Y_new = np.where(Y_array==20, ACT_OBJ_WILDCARD, Y_new)
-      Y_new = np.where(Y_array==19, ACT_IDLE,         Y_new)
-      Y_new = np.where(Y_array==18, ACT_IDLE,         Y_new)
-      Y_new = np.where(Y_array==17, ACT_PAYING_CHECK, Y_new)
-      Y_new = np.where(Y_array==16, ACT_OBJ_WILDCARD, Y_new)
-      Y_new = np.where(Y_array==15, ACT_OBJ_WILDCARD, Y_new)
-      Y_new = np.where(Y_array==14, ACT_OBJ_WILDCARD, Y_new)
-      Y_new = np.where(Y_array==13, ACT_USING_PHONE,  Y_new)
-      Y_new = np.where(Y_array==12, ACT_PAYING_CHECK, Y_new)
-      Y_new = np.where(Y_array==11, ACT_READING_MENU, Y_new)
-      Y_new = np.where(Y_array==10, ACT_READING_BILL, Y_new)
-      Y_new = np.where(Y_array==9, ACT_WAITER,        Y_new) 
-      Y_new = np.where(Y_array==8, ACT_LOOKING_WINDOW,Y_new)
-      Y_new = np.where(Y_array==7, ACT_WAITER,        Y_new)
-      Y_new = np.where(Y_array==6, ACT_STANDING,            Y_new)
-      Y_new = np.where(Y_array==5, ACT_WAITER,        Y_new)
-      Y_new = np.where(Y_array==4, ACT_TALKING,             Y_new)
-      Y_new = np.where(Y_array==3, ACT_EATING,        Y_new)
-      Y_new = np.where(Y_array==2, ACT_EATING,        Y_new)
-      Y_new = np.where(Y_array==1, ACT_IDLE,                Y_new)
-      Y_new = np.where(Y_array==0, ACT_AWAY_FROM_TABLE, Y_new)
+# def reduce_labels(Y_array, feature_type):
+#     # activity_from_key = {0:'away-from-table', 1:'idle', 2:'eating', 3: 'drinking', 4: 'talking', 5: 'ordering', 6: 'standing',
+#     #                     7: 'talking:waiter', 8: 'looking:window', 9: 'looking:waiter', 10: 'reading:bill', 11: 'reading:menu',
+#     #                     12: 'paying:check', 13: 'using:phone', 14: 'using:napkin', 15: 'using:purse', 16: 'using:glasses',
+#     #                     17: 'using:wallet', 18: 'looking:PersonA', 19: 'looking:PersonB', 20: 'takeoutfood', 21: 'leaving-table', 22: 'cleaning-up', 23: 'NONE'}
 
-      return Y_new
+#     # activity_labels = [0: 'NONE', 1: 'away-from-table', 2: 'idle', 3: 'eating', 4: 'talking', 5:'talking:waiter', 6: 'looking:window', 
+#     # 7: 'reading:bill', 8: 'reading:menu', 9: 'paying:check', 10: 'using:phone', 11: 'using:objs', 12: 'standing']
+    
+#     ACT_NONE            = 0
+#     ACT_AWAY_FROM_TABLE     = 1
+#     ACT_IDLE            = 2
+#     ACT_EATING          = 3
+#     ACT_TALKING         = 4
+#     ACT_WAITER          = 5
+#     ACT_LOOKING_WINDOW    = 6
+#     ACT_READING_BILL      = 7
+#     ACT_READING_MENU      = 8
+#     ACT_PAYING_CHECK      = 9
+#     ACT_USING_PHONE       = 10
+#     ACT_OBJ_WILDCARD      = 11
+#     ACT_STANDING        = 12
+#     ACT_DRINKING          = 13
+
+#     Y_new = np.empty_like(Y_array)
+#     Y_new = np.where(Y_array==23, ACT_NONE,       Y_new)
+#     Y_new = np.where(Y_array==22, ACT_NONE,       Y_new)
+#     Y_new = np.where(Y_array==21, ACT_STANDING,     Y_new) 
+#     Y_new = np.where(Y_array==20, ACT_OBJ_WILDCARD, Y_new)
+#     Y_new = np.where(Y_array==19, ACT_IDLE,       Y_new)
+#     Y_new = np.where(Y_array==18, ACT_IDLE,       Y_new)
+#     Y_new = np.where(Y_array==17, ACT_PAYING_CHECK, Y_new)
+#     Y_new = np.where(Y_array==16, ACT_OBJ_WILDCARD, Y_new)
+#     Y_new = np.where(Y_array==15, ACT_OBJ_WILDCARD, Y_new)
+#     Y_new = np.where(Y_array==14, ACT_OBJ_WILDCARD, Y_new)
+#     Y_new = np.where(Y_array==13, ACT_USING_PHONE,  Y_new)
+#     Y_new = np.where(Y_array==12, ACT_PAYING_CHECK, Y_new)
+#     Y_new = np.where(Y_array==11, ACT_READING_MENU, Y_new)
+#     Y_new = np.where(Y_array==10, ACT_READING_BILL, Y_new)
+#     Y_new = np.where(Y_array==9, ACT_WAITER,      Y_new) 
+#     Y_new = np.where(Y_array==8, ACT_LOOKING_WINDOW,Y_new)
+#     Y_new = np.where(Y_array==7, ACT_WAITER,      Y_new)
+#     Y_new = np.where(Y_array==6, ACT_STANDING,        Y_new)
+#     Y_new = np.where(Y_array==5, ACT_WAITER,      Y_new)
+#     Y_new = np.where(Y_array==4, ACT_TALKING,         Y_new)
+#     Y_new = np.where(Y_array==3, ACT_EATING,      Y_new)
+#     Y_new = np.where(Y_array==2, ACT_EATING,      Y_new)
+#     Y_new = np.where(Y_array==1, ACT_IDLE,            Y_new)
+#     Y_new = np.where(Y_array==0, ACT_AWAY_FROM_TABLE, Y_new)
+
+#     return Y_new
+
+def reduce_Y_labels(df):
+    # print("Pre")
+    # print(df[PD_LABEL_A_CODE].unique())
+    # print(df[PD_LABEL_B_CODE].unique())
+
+    df[PD_LABEL_A_CODE] = df[PD_LABEL_A_CODE].map(transform_dict)
+    df[PD_LABEL_B_CODE] = df[PD_LABEL_B_CODE].map(transform_dict)
+
+    # print("post")
+    # print(df[PD_LABEL_A_CODE].unique())
+    # print(df[PD_LABEL_B_CODE].unique())
+
+    return df
+
+def dont_reduce_Y_labels(df):
+    # print("Pre")
+    # print(df[PD_LABEL_A_CODE].unique())
+    # print(df[PD_LABEL_B_CODE].unique())
+
+    mini_transform = {OG_LABEL_NA: OG_LABEL_NONE}
+    df = df.replace({PD_LABEL_A_CODE: mini_transform})
+    df = df.replace({PD_LABEL_B_CODE: mini_transform})
+
+    # print("post")
+    # print(df[PD_LABEL_A_CODE].unique())
+    # print(df[PD_LABEL_B_CODE].unique())
+
+    return df
+
 
 def get_start_time():
-      time_start = time.perf_counter()
-      return time_start
+    time_start = time.perf_counter()
+    return time_start
 
 def print_time(time_start):
-      time_end = time.perf_counter()
-      time_diff = time_end - time_start
+    time_end = time.perf_counter()
+    time_diff = time_end - time_start
 
-      now = datetime.now()
-      current_time = now.strftime("%H:%M:%S")
-      print("Time elapsed: " + str(time_diff) + " ending at " + str(current_time))
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Time elapsed: " + str(time_diff) + " ending at " + str(current_time))
